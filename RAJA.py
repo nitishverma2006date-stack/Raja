24f65f846392c32d85b89aa7614d100f27c997f0
nitishverma2006date-stack <nitishverma2006date@gmail.com>
2026-03-09 20:37:11 +0530

    Add files via upload

---
+++ RAJA.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8381386679:AAH2wRKtiGNs12Ka5YPFjE-tvRw39RG0Lkc")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Ninjaytx")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5829441229")
+
+
+
+
+
+
+
+  
